fa13e47c6598e537bfbeab04e3cbd2176e62ea8a
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-06-27 00:19:28 +0300

    added boilerplate

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+
+const isAuth = !!localStorage.getItem(process.env.REACT_APP_ACCESS_TOKEN_PATH);
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuth ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+}
+
+export default PrivateRoute;
---
+++ src/store/auth.js
@@ -0,0 +1,20 @@
+import { thunk } from "easy-peasy";
+import history from "../lib/history";
+
+import { API } from "./index";
+
+export default {
+  authLoading: false,
+  login: thunk(async (_, payload) => {
+    const response = await API.postLogin(payload);
+    localStorage.setItem(
+      process.env.REACT_APP_ACCESS_TOKEN_PATH,
+      response.data.access_token
+    );
+    history.replace("/");
+  }),
+  logout: thunk(() => {
+    localStorage.removeItem(process.env.REACT_APP_ACCESS_TOKEN_PATH);
+    history.replace("/login");
+  })
+};
